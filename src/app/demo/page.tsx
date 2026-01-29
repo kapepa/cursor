80ce8ff072dbcd04925fb8bcc18a098e2754e970
kapepa <kapepayx@gmail.com>
2026-01-29 16:17:18 +0200

    error

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
+import { useAuth } from "@clerk/nextjs";
 import { NextPage } from "next";
 import { useTransition } from "react";
 
 const DemoPage: NextPage = () => {
+  const { userId } = useAuth();
   const [isPendingBlocking, startTransitionBlocking] = useTransition()
   const [isPendingBackground, startTransitionBackground] = useTransition()
   const handlerBlocking = async () => {
@@ -16,6 +18,23 @@ const DemoPage: NextPage = () => {
   const handlerBackground = async () => {
     startTransitionBackground(async () => {
       await fetch("/api/demo/background", { method: "POST" })
+    })
+  }
+
+  const handleClientError = () => {
+    console.log(userId)
+    throw new Error("Client error: Something went wrong in the browser!")
+  }
+
+  const handleApiError = () => {
+    startTransitionBackground(async () => {
+      await fetch("/api/demo/error", { method: "POST" })
+    })
+  }
+
+  const handleInngestError = () => {
+    startTransitionBackground(async () => {
+      await fetch("/api/demo/inngest-error", { method: "POST" })
     })
   }
 
@@ -34,6 +53,24 @@ const DemoPage: NextPage = () => {
         onClick={handlerBackground}
       >
         {isPendingBackground ? "Loading..." : "Background"}
+      </Button>
+      <Button
+        variant="destructive"
+        onClick={handleClientError}
+      >
+        Clent Error
+      </Button>
+      <Button
+        variant="destructive"
+        onClick={handleApiError}
+      >
+        API Error
+      </Button>
+      <Button
+        variant="destructive"
+        onClick={handleInngestError}
+      >
+        Inngest Error
       </Button>
     </div>
   )

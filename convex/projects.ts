9b6c80e39dec4c00b79f23c453520c66c4858e61
kapepa <kapepayx@gmail.com>
2026-02-03 08:03:07 +0200

    add project, view

--- convex/projects.ts
+++ convex/projects.ts
@@ -1,29 +1,48 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
+import { verifyAuth } from "./auth";
 
 export const create = mutation({
   args: {
     name: v.string()
   },
   handler: async (ctx, args) => {
-    const indentity = await ctx.auth.getUserIdentity();
-    if (!indentity) throw new Error("Unauthorized");
+    const indentity = await verifyAuth(ctx);
 
-    await ctx.db.insert("projects", {
+    const project = await ctx.db.insert("projects", {
       name: args.name,
-      ownerId: indentity.subject
+      ownerId: indentity.subject,
+      updatedAt: Date.now()
     });
+
+    return project;
   },
 });
+
+export const getPartial = query({
+  args: {
+    limit: v.number(),
+  },
+  handler: async (ctx, args) => {
+    const indentity = await verifyAuth(ctx);
+
+    return await ctx.db
+      .query("projects")
+      .withIndex("by_owner", (q) => q.eq("ownerId", indentity.subject))
+      .order("desc")
+      .take(args.limit)
+  }
+})
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    const indentity = await ctx.auth.getUserIdentity();
-    if (!indentity) return [];
+    const indentity = await verifyAuth(ctx);
 
-    return await ctx.db.query("projects")
+    return await ctx.db
+      .query("projects")
       .withIndex("by_owner", (q) => q.eq("ownerId", indentity.subject))
+      .order("desc")
       .collect()
   }
 })
